7806affd54b05813fa9b24debf507648cde4da5d
Irina <irina6793@yahoo.com>
2020-02-13 08:59:32 -0500

    working on 1st question

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,12 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
- hello
+   let a = 0;
+   let b = 1; /*need to have two variables in the sequence */
+
+   while(true) {
+       const c = a + b  /* the while loop will go through the numbers */
+       
+   }
+
 }
