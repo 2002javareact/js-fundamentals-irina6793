d630ffc2ddb57ffcf9a7464a1677c5d955bb4ebf
Irina <irina6793@yahoo.com>
2020-02-13 10:11:36 -0500

    in progress

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,13 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+  let isPalindrome = 0;
+  let strLen = someStr.length
+  if(strLen === 0 || strLen ===1) {
+      return true;
+  }
+  if(someStr[0] === someStr[strLen-1]) {
+      return isPalindrome(someStr.slice)
+  }
+
 }
