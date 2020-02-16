d1a29eb488e20e1a78236198e4d07ced41c2069a
Irina <irina6793@yahoo.com>
2020-02-15 21:41:36 -0500

    q10 completed

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,25 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let info = {
+    name: 'irina',
+    age: 28
+}
+console.log(info.name, info.age);
+
+function info(name, age) {
+    this.name=name;
+    this.age=age;
+}
+let contact = new info("dasha", 24);
+console.log(contact.name, contact.age);
+
+class info {
+    constructor(name, age) {
+        this.name=name
+        this.age=age
+    }
+    let contacts = new info ('galina', 56)
+    console.log(contacts.name, contacts.age)
+}
